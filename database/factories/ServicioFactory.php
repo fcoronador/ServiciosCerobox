4dcf9833e3e2487d614d035be401cf6f9aa14771
Fredy Coronado <ing.ambfredycoronado@gmail.com>
2020-10-23 09:30:10 -0500

    se inicia proyecto

---
+++ database/factories/ServicioFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\servicio;
+use Faker\Generator as Faker;
+
+$factory->define(servicio::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
