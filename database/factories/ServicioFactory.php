4d4d8b3bb36060ba2a16e50f02aee00c62f76b19
Fredy Coronado <ing.ambfredycoronado@gmail.com>
2020-10-23 10:15:18 -0500

    se agrega factory para servicio

--- database/factories/ServicioFactory.php
+++ database/factories/ServicioFactory.php
@@ -4,9 +4,15 @@
 
 use App\servicio;
 use Faker\Generator as Faker;
+use phpDocumentor\Reflection\PseudoTypes\True_;
 
 $factory->define(servicio::class, function (Faker $faker) {
     return [
-        //
+        'nombre' => $faker->name,
+        'img' => 'https://img.icons8.com/ios/452/laravel.png',
+        'Tservcio' => true,
+        'FechaIni' => $faker->date($format = 'Y/m/d', $max = 'now'),
+        'FechaFin' => $faker->date($format = 'Y/m/d', $max = 'now'),
+        'obser' => $faker->text($maxNbChars = 200)
     ];
 });
