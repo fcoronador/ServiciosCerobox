1328eeb081c04a5a10b92e8b78e803a336f48227
Fredy Coronado <ing.ambfredycoronado@gmail.com>
2020-10-23 09:51:51 -0500

    se crea el modelo de servicios

--- app/servicio.php
+++ app/servicio.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class servicio extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'nombre',
+        'img',
+        'Tservcio',
+        'FechaIni',
+        'FechaFin',
+        'obser'
+    ];
+    protected $dates = ['deleted_at'];
 }
